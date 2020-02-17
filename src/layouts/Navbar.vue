53aefbbb5e14ed221cd968f0f02e15aba8f7f08c
zhongq0930 <836159458@qq.com>
2020-02-17 18:23:41 +0800

    完成 navbar

---
+++ src/layouts/Navbar.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <b-navbar toggleable="lg" type="dark" variant="dark">
+      <b-container>
+        <b-navbar-brand tag="h1" class="mb-0" href="/">Mark</b-navbar-brand>
+
+        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+        <b-collapse id="nav-collapse" is-nav>
+          <b-navbar-nav>
+            <b-nav-item href="/">女优</b-nav-item>
+            <b-nav-item href="#" active>影片</b-nav-item>
+            <b-nav-item href="#">系列</b-nav-item>
+          </b-navbar-nav>
+
+          <!-- Right aligned nav items -->
+          <b-navbar-nav class="ml-auto">
+            <b-nav-form>
+              <b-form-input size="sm" class="mr-sm-2" placeholder="Search"></b-form-input>
+              <b-button size="sm" class="my-2 my-sm-0" type="submit">搜索</b-button>
+            </b-nav-form>
+          </b-navbar-nav>
+        </b-collapse>
+      </b-container>
+    </b-navbar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Navbar'
+}
+</script>
+
+<style scoped>
+
+</style>
