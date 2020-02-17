c4b70bb301622851b22b5068666cf850b162bd24
zhongq0930 <836159458@qq.com>
2020-02-17 16:05:31 +0800

    引入 bootstrap-vue

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,11 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <p>
+      If bootstrap-vue is successfully added to this project, you'll see an
+      <code v-text="'<b-button>'"></code>
+      below
+    </p>
+    <b-button variant="success">Button</b-button>
   </div>
 </template>
